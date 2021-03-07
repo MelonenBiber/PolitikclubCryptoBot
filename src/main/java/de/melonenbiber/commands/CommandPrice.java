a9bd7177edacba7541d8ef246dbc782586f164d0
MelonenBiber <45320352+MelonenBiber@users.noreply.github.com>
2021-03-08 00:07:05 +0100

    Rewrote class; Looks way less ugly now

--- src/main/java/de/melonenbiber/commands/CommandPrice.java
+++ src/main/java/de/melonenbiber/commands/CommandPrice.java
@@ -1,11 +1,13 @@
 package de.melonenbiber.commands;
 
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.User;
 import de.melonenbiber.utils.ApiCallException;
 import de.melonenbiber.utils.CoingeckoApiUtils;
 import de.melonenbiber.utils.Cryptocurrency;
+import de.melonenbiber.utils.UserInputUtils;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
 
+import java.util.Optional;
 import java.util.Random;
 
 public class CommandPrice extends CommandBase
@@ -19,33 +21,41 @@ public class CommandPrice extends CommandBase
     @Override
     public boolean execute(String[] args, User author, MessageChannel channel)
     {
-        String currency = args.length > 1 ? args[1] : "usd";
+        String cryptoCurrencyName = args[0];
+        String currencyName = args.length > 1 ? args[1] : "usd";
+
+        // Validate Cryptocurrencyname
+        Optional<String> optionalCryptoCurrencyName = UserInputUtils.convertCryptoCurrencyInputToId(cryptoCurrencyName);
+
+        if (optionalCryptoCurrencyName.isEmpty())
+        {
+            channel.sendMessage("Unknown cryptocurrency '" + args[0] + "'").queue();
+            return true;
+        }
+
+        String cryptoCurrencyId = optionalCryptoCurrencyName.get();
+
+        // Validate Currencyname
+        if (!UserInputUtils.isCurrencyValid(currencyName))
+        {
+            channel.sendMessage("Unknown currency '" + currencyName + "'").queue();
+            return true;
+        }
+
 
         try
         {
-            Cryptocurrency c = CoingeckoApiUtils.getCryptocurrency(args[0]);
+            Cryptocurrency c = CoingeckoApiUtils.getCryptocurrency(cryptoCurrencyId);
 
-            double price;
-            try
-            {
-                price = c.market_data.current_price.get(currency);
-            } catch (NullPointerException e)
-            {
-                channel.sendMessage("Currency '" + currency + "' not found.").queue();
-                return true;
-            }
-
-            if (new Random().nextInt(20) % 20 == 0 && author.getId().equalsIgnoreCase("205440355452190720"))
-            {
+            if (new Random().nextInt(5) % 5 == 0 && author.getId().equalsIgnoreCase("205440355452190720"))
                 channel.sendMessage("Elena du bist deppert!").queue();
-            }
 
-            channel.sendMessage(price + " " + currency).queue();
-
+            channel.sendMessage(c.market_data.current_price.get(currencyName) + " " + currencyName).queue();
+            return true;
         } catch (ApiCallException e)
         {
             channel.sendMessage(e.getMessage()).queue();
+            return true;
         }
-        return true;
     }
 }
