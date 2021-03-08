1b8ad8faa9719158bf92bc39b3957a7125e23446
MelonenBiber <45320352+MelonenBiber@users.noreply.github.com>
2021-03-08 13:31:07 +0100

    Add easy currency validation

--- src/main/java/de/melonenbiber/utils/UserInputUtils.java
+++ src/main/java/de/melonenbiber/utils/UserInputUtils.java
@@ -1,9 +1,14 @@
 package de.melonenbiber.utils;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public class UserInputUtils
 {
+
+    private static final List<String> validCurrencies = Arrays.asList("aed", "ars", "aud", "bch", "bdt", "bhd", "bmd", "bnb", "brl", "btc", "cad", "chf", "clp", "cny", "czk", "dkk", "dot", "eos", "eth", "eur", "gbp", "hkd", "huf", "idr", "ils", "inr", "jpy", "krw", "kwd", "lkr", "ltc", "mmk", "mxn", "myr", "ngn", "nok", "nzd", "php", "pkr", "pln", "rub", "sar", "sek", "sgd", "thb", "try", "twd", "uah", "usd", "vef", "vnd", "xag", "xau", "xdr", "xlm", "xrp", "yfi", "zar", "bits", "link", "sats");
+
 
     public static Optional<String> convertCryptoCurrencyInputToId(String input)
     {
@@ -17,5 +22,10 @@ public class UserInputUtils
 
         }
         return Optional.empty();
+    }
+
+    public static boolean isCurrencyValid(String currencyName)
+    {
+        return validCurrencies.contains(currencyName);
     }
 }
